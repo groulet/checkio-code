6db84c61a43c8ac34bc511189b1d6f7e17e5d389
groulet <gilles.roulet@gmail.com>
2020-03-31 10:06:03 -0400

    added split-list

---
+++ home/split-list/solve.py
@@ -0,0 +1,33 @@
+'''
+You have to split a given array into two arrays. If it has an odd amount of elements, then the first array should have more elements. If it has no elements, then two empty arrays should be returned.
+
+example
+
+Input: Array.
+
+Output: Array or two arrays.
+
+Example:
+
+split_list([1, 2, 3, 4, 5, 6]) == [[1, 2, 3], [4, 5, 6]]
+split_list([1, 2, 3]) == [[1, 2], [3]]
+'''
+def split_list(items: list) -> list:
+    # len(items) + 1 for round down conversion (2+1)/2 = 1.5 => int(1)
+    # // is int division
+    # slice the array
+    return [items[:(len(items)+1)//2],items[(len(items)+1)//2:]]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(split_list([1, 2, 3, 4, 5, 6]))
+    print(split_list([1, 2, 3]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert split_list([1, 2, 3, 4, 5, 6]) == [[1, 2, 3], [4, 5, 6]]
+    assert split_list([1, 2, 3]) == [[1, 2], [3]]
+    assert split_list([1, 2, 3, 4, 5]) == [[1, 2, 3], [4, 5]]
+    assert split_list([1]) == [[1], []]
+    assert split_list([]) == [[], []]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
