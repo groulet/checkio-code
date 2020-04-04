53e0850e1ba17130721211c5b13e90f88a2d7e3c
Gilles <giles.roulet@gmail.com>
2020-04-04 14:42:35 -0400

    added sum-by-type

---
+++ scientific-expedition/sum-by-type/solve.py
@@ -0,0 +1,44 @@
+'''
+https://py.checkio.org/en/mission/sum-by-type/
+
+You have a list. Each value from that list can be either a string or an integer.
+Your task here is to return two values. The first one is a concatenation of all
+strings from the given list. The second one is a sum of all integers from the given list.
+
+Input: An array of strings ans integers
+
+Output: A list or tuple
+
+Example:
+
+sum_by_types([]) == ['', 0]
+sum_by_types([1, 2, 3]) == ['', 6]
+sum_by_types(['1', 2, 3]) == ['1', 5]
+sum_by_types(['1', '2', 3]) == ['12', 3]
+sum_by_types(['1', '2', '3']) == ['123', 0]
+sum_by_types(['size', 12, 'in', 45, 0]) == ['sizein', 57]
+
+How it’s used: (math is used everywhere)
+
+Precondition: both given ints should be between -1000 and 1000
+'''
+
+from typing import Tuple
+from functools import reduce
+import operator
+def sum_by_types(items: list) -> Tuple[str, int]:
+    return (reduce(operator.add,filter(lambda x: type(x)==str,items),''), reduce(operator.add,filter(lambda x: type(x)==int,items),0))
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(sum_by_types([]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert sum_by_types([]) == ('', 0)
+    assert sum_by_types([1, 2, 3]) == ('', 6)
+    assert sum_by_types(['1', 2, 3]) == ('1', 5)
+    assert sum_by_types(['1', '2', 3]) == ('12', 3)
+    assert sum_by_types(['1', '2', '3']) == ('123', 0)
+    assert sum_by_types(['size', 12, 'in', 45, 0]) == ('sizein', 57)
+    print("Coding complete? Click 'Check' to earn cool rewards!")
